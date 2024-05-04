0a3d5ad2c7aec121133fc881873611b0c194dfbd
Nika K <firstnamenika@gmail.com>
2024-05-04 17:30:00 -0400

    added routes for MainLayout, HomePage, JobsPage and NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
+import { Route, createBrowserRouter ,createRoutesFromElements, RouterProvider } from 'react-router-dom'; 
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+import NotFound from './pages/notFound';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      {/* // create a router with a path and display this element */}
+      <Route index element={ <HomePage /> } /> 
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFound />} />
+        
+    </Route>) 
+  
+);
 
 function App() {
-  return (
-    <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
-    </>
-  )
+  return <RouterProvider router={router} />;
 };
 
 export default App;
